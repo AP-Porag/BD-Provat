cc420c25a7bd14856d0455ba1b0a935d4e346562
AP-Porag <smauhp@gmail.com>
2021-01-01 22:17:17 +0600

    Initial Commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.website')
+
+@section('title')
+BD-Provat
+@endsection
+
+@section('style')
+    <style>
+
+    </style>
+@endsection
+
+@section('content')
+    <div class="content">
+
+    </div>
+@endsection
+
+@section('script')
+    <script>
+
+    </script>
+@endsection
