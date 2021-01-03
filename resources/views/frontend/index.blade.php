d0ccc724867d296b3a5d1de19d51685ec839b6f8
RAZAUL <razulk127@gmail.com>
2021-01-03 13:48:37 +0600

    Commit main branch

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -119,7 +119,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8"></div>
-                <div class="col-md-4"></div>
+                <div class="col-md-4">Razaul Karim</div>
             </div>
         </div>
     </section>
