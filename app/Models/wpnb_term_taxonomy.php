f25d3aa07d6c7663b73d9dbd1adea0eca57223d4
AP-Porag <smauhp@gmail.com>
2021-01-05 20:10:20 +0600

    Database Integration Completed

---
+++ app/Models/wpnb_term_taxonomy.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class wpnb_term_taxonomy extends Model
+{
+    //
+}
--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,//this should be true , I changed it for term texonomy problem
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
