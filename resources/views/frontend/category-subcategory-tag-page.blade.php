c819ab71d0d346682bd4f3d42e53b347d0d983ac
RAZAUL <razulk127@gmail.com>
2021-01-23 20:50:49 +0600

    UpdatePaginationAndResponsive

--- resources/views/frontend/category-subcategory-tag-page.blade.php
+++ resources/views/frontend/category-subcategory-tag-page.blade.php
@@ -21,7 +21,7 @@
                             <img src="{{ $main_post->thumbnail }}" class="w-100 p-1" style="border: 1px solid #999">
                             <h4 class="mt-3">{{ $main_post->title }}</h4>
                             <p style="text-align:justify">
-                                {{ Illuminate\Support\Str::of($main_post->content)->words(30) }}
+                                {!! Illuminate\Support\Str::of($main_post->content)->words(30) !!}
                             </p>
                         </a>
                     </div>
@@ -53,29 +53,33 @@
                     @endforeach
                 </div>
                 <div class="row">
-                    @foreach ($posts as $key => $post)
-                        <a href="{{ route('single-post-page', $post->slug) }}">
-                            <div class="col-md-12 d-flex p-2 mb-3" style="background: #EDF3F5">
-                                <div class="img">
-                                    <img src="{{ $post->thumbnail }}" class="img-fluid p-1" style="border: 1px solid #fff">
+                    <div class="col-md-12">
+                        @foreach ($posts as $key => $post)
+                            <a href="{{ route('single-post-page', $post->slug) }}">
+                                <div class="col-md-12 d-flex p-2 mb-3" style="background: #EDF3F5">
+                                    <div class="img">
+                                        <img src="{{ $post->thumbnail }}" class="img-fluid p-1"
+                                            style="border: 1px solid #fff">
+                                    </div>
+                                    <div class="title_details p-4">
+                                        <h4 class="mt-1">{{ $right_side_post->title }}</h4>
+                                        <p style="text-align:justify">
+                                            {!! Illuminate\Support\Str::of($main_post->content)->words(30) !!}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div class="title_details p-4">
-                                    <h4 class="mt-1">{{ $right_side_post->title }}</h4>
-                                    <p style="text-align:justify">
-                                        {{ Illuminate\Support\Str::of($main_post->content)->words(30) }}
-                                    </p>
-                                </div>
-                            </div>
 
-                        </a>
-                    @endforeach
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
                 {{ $posts->links() }}
 
 
             </div>
         </div>
-        <div class="col-md-4"></div>
+        <div class="col-md-4">
+        </div>
     </div>
     {{-- Main news section end --}}
 @endsection
